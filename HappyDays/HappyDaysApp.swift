c92ebddb4e3bdc5bfbce6b29adcecf06f571ff97
Philipp <pd95@users.noreply.github.com>
2020-08-08 11:16:18 +0200

    Initial Commit

---
+++ HappyDays/HappyDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyDaysApp.swift
+//  HappyDays
+//
+//  Created by Philipp on 08.08.20.
+//
+
+import SwiftUI
+
+@main
+struct HappyDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
